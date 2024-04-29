d5ddd72ec95888052b5c831c7ed804c41c4a1a8f
mahboobehgh72 <145811881+mahboobehgh72@users.noreply.github.com>
2024-04-29 21:54:09 +0330

    Add files via upload

---
+++ text.js
@@ -0,0 +1,23 @@
+
+const app = Vue.createApp({
+    data(){
+        return{
+
+            title:"add",
+            box:"remove",
+            mount:0,
+        }
+        
+    } , 
+    methods:{ 
+        chengeTitel(numbers){
+         this.title=number ;          
+        }
+    },
+
+    }
+    
+)
+
+app.mount("#app");
+
